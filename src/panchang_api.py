342e08596cc61dba9287488877197df60d9e456c
vidyarthilk <vidyarthilalkitab@gmail.com>
2025-08-01 22:00:57 +0530

    Update panchang_api.py

--- src/panchang_api.py
+++ src/panchang_api.py
@@ -59,8 +59,9 @@ def calculate():
         swe.set_sid_mode(swe.SIDM_LAHIRI)
 
         # Tithi
-        sun_long = swe.calc_ut(jd, swe.SUN)[0]
-        moon_long = swe.calc_ut(jd, swe.MOON)[0]
+        sun_long = swe.calc_ut(jd, swe.SUN)[0][0]
+        moon_long = swe.calc_ut(jd, swe.MOON)[0][0]
+
       
         print("DEBUG sun_long:", sun_long, type(sun_long))
         print("DEBUG moon_long:", moon_long, type(moon_long))
